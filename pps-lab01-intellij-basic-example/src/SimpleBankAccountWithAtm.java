8285a8a68d1947743a85db7d23e400a6266be244
andrea.giulianelli <andrea.giulianelli4@studio.unibo.it>
2022-02-28 11:02:54 +0100

    green - deposit with atm

--- pps-lab01-intellij-basic-example/src/SimpleBankAccountWithAtm.java
+++ pps-lab01-intellij-basic-example/src/SimpleBankAccountWithAtm.java
@@ -1,6 +1,51 @@
 import lab01.example.model.AccountHolder;
+import lab01.example.model.BankAccount;
 
-public class SimpleBankAccountWithAtm {
-    public SimpleBankAccountWithAtm(AccountHolder accountHolder, int i) {
+public class SimpleBankAccountWithAtm implements BankAccount {
+    public static final int FEE = 1;
+
+    private double balance;
+    private final AccountHolder holder;
+
+    public SimpleBankAccountWithAtm(final AccountHolder holder, final double balance) {
+        this.holder = holder;
+        this.balance = balance;
+    }
+    @Override
+    public AccountHolder getHolder(){
+        return this.holder;
+    }
+
+    @Override
+    public double getBalance() {
+        return this.balance;
+    }
+
+    @Override
+    public void deposit(final int userID, final double amount) {
+        if (checkUser(userID)) {
+            this.balance += amount;
+        }
+    }
+
+    @Override
+    public void withdraw(final int userID, final double amount) {
+        if (checkUser(userID) && isWithdrawAllowed(amount)) {
+            this.balance -= amount;
+        }
+    }
+
+    public void depositWithAtm(final int userID, final double amount) {
+        if (checkUser(userID)) {
+            this.balance += amount - FEE;
+        }
+    }
+
+    private boolean isWithdrawAllowed(final double amount){
+        return this.balance >= amount;
+    }
+
+    private boolean checkUser(final int id) {
+        return this.holder.getId() == id;
     }
 }

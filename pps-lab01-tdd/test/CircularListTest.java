d615f605e0a13e0067209cf71b0c79962a28fa94
andrea.giulianelli <andrea.giulianelli4@studio.unibo.it>
2022-03-01 14:36:07 +0100

    test next and previous initially empty

--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -3,6 +3,9 @@ import lab01.tdd.CircularList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -25,5 +28,15 @@ public class CircularListTest {
     void addElementIncreaseSize() {
         this.circularList.add(10);
         assertTrue(!this.circularList.isEmpty() && this.circularList.size() == 1);
+    }
+
+    @Test
+    void nextInitiallyEmpty() {
+        assertEquals(Optional.empty(), this.circularList.next());
+    }
+
+    @Test
+    void previousInitiallyEmpty() {
+        assertEquals(Optional.empty(), this.circularList.previous());
     }
 }

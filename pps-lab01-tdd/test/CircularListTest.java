b23e517bc40a8e9fff45e2478305edc3d2ecd090
andrea.giulianelli <andrea.giulianelli4@studio.unibo.it>
2022-03-01 14:25:35 +0100

    red - test initially empty

--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,17 +1,22 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
+import lab01.tdd.CircularList;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
+    private CircularList circularList;
 
-    //TODO: test implementation
-
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+    @BeforeEach
+    void setUp() {
+        this.circularList = new BasicCircularList();
     }
 
+    @Test
+    void isInitiallyEmpty() {
+        assertTrue(this.circularList.isEmpty());
+    }
 }
